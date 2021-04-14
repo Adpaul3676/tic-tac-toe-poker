0cb1361f112e5ac78073ee3449bc4f4c87c9c29e
adpaul3676 <adpaul3676@gmail.com>
2021-04-14 17:07:53 -0600

    FIxed it!

--- Part1/index.js
+++ Part1/index.js
@@ -1,22 +1,10 @@
-
 let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-
-let top1 = arr[0];
-let top2 = arr[1];
-let top3 = arr[2];
-let middle1 = arr[3];
-let middle2 = arr[4];
-let middle3 = arr[5];
-let bottom1 = arr[6];
-let bottom2 = arr[7];
-let bottom3 = arr[8];
-
 let player = document.getElementById('player');
 
 function play(x) {
-
+  
   let clickedBox = document.getElementById(x);
-
+  
   if (clickedBox.innerText === 'X') {
     alert ("You can't do that!")
     return
@@ -24,15 +12,28 @@ function play(x) {
     alert ("You can't do that!")
     return
   } else if (player.innerText === 'X') {
-    arr[x] = 'X'
+    arr.splice (x, 1, 'X')
     clickedBox.innerText = player.innerText
     player.innerText = 'O'
   } else {
-    arr[x] = 'O'
+    arr.splice (x, 1, 'O')
     clickedBox.innerText = player.innerText
     player.innerText = 'X'
   }
+
+  let top1 = arr[0];
+  let top2 = arr[1];
+  let top3 = arr[2];
+  let middle1 = arr[3];
+  let middle2 = arr[4];
+  let middle3 = arr[5];
+  let bottom1 = arr[6];
+  let bottom2 = arr[7];
+  let bottom3 = arr[8];
+  
   console.log (arr);  
+  console.log (top1, top2, top3);
+  // console.log (middle1, middle2, middle3); 
 
   if (top1 === top2 && top1 === top3) {
       alert (top1 + ' wins!');
